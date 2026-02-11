048083136596142eda103bfd27ea094b223d7976
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-11 09:25:39 -0600

    Feat: Se agrego la logica de la entidad Repuestos

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/entity/Repuestos.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/entity/Repuestos.java
@@ -1,4 +1,79 @@
 package com.mateoortiz.repuestosAutomotrices.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Repuestos")
 public class Repuestos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    @Column(name = "nombre_repuesto")
+    private String nombreRepuesto;
+
+    @Column(name = "categoria_repuesto")
+    private String categoriaRepuesto;
+
+    @Column(name = "precio_compra")
+    private Double precioCompra;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    // Relación con Proveedor
+    @ManyToOne
+    @JoinColumn(name = "id_proveedor", nullable = false)
+    private Proveedor proveedor;
+
+    // Getters y Setters
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
+
+    public String getNombreRepuesto() {
+        return nombreRepuesto;
+    }
+
+    public void setNombreRepuesto(String nombreRepuesto) {
+        this.nombreRepuesto = nombreRepuesto;
+    }
+
+    public String getCategoriaRepuesto() {
+        return categoriaRepuesto;
+    }
+
+    public void setCategoriaRepuesto(String categoriaRepuesto) {
+        this.categoriaRepuesto = categoriaRepuesto;
+    }
+
+    public Double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(Double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
 }
