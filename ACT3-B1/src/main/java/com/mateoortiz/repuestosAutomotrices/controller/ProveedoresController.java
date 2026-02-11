70ef7d48045030deba5df2e66479fbcb86d15509
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-10 20:51:49 -0600

    Feat: Se agrego la logica de ProveedoresController

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/controller/ProveedoresController.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/controller/ProveedoresController.java
@@ -1,4 +1,51 @@
 package com.mateoortiz.repuestosAutomotrices.controller;
 
+import com.mateoortiz.repuestosAutomotrices.entity.Proveedor;
+import com.mateoortiz.repuestosAutomotrices.service.ProveedoresService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/proveedores")
+
 public class ProveedoresController {
+    private final ProveedoresService proveedoresService;
+
+    public ProveedoresController(ProveedoresService proveedoresService){ this.proveedoresService=proveedoresService; }
+
+    @GetMapping
+    public List<Proveedor> getAllProveedores(){ return proveedoresService.getAllProveedores(); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProveedorById (@PathVariable Integer id) {
+        Proveedor proveedor = proveedoresService.getProveedorById(id);
+        return ResponseEntity.ok(proveedor);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createProveedor(@Valid @RequestBody Proveedor proveedor){
+        try{
+            Proveedor createdProveedor = proveedoresService.saveProveedor(proveedor);
+            return new ResponseEntity<>(createdProveedor, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Proveedor> updateProveedor(@PathVariable Integer id, @RequestBody Proveedor proveedor) {
+        Proveedor updatedProveedor = proveedoresService.updateProveedor(id, proveedor);
+        return ResponseEntity.ok(updatedProveedor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProveedor(@PathVariable Integer id){
+        proveedoresService.deleteProveedor(id);
+        return ResponseEntity.ok().build();
+    }
 }
