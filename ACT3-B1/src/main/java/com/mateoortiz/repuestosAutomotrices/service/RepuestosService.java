ac67d68b031f95f4aa955adf21fe7760daacde30
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-11 09:54:28 -0600

    Feat: Se agrego la logica de RepuestosService

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/RepuestosService.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/RepuestosService.java
@@ -1,4 +1,20 @@
 package com.mateoortiz.repuestosAutomotrices.service;
 
+import com.mateoortiz.repuestosAutomotrices.entity.Repuestos;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface RepuestosService {
+
+    List<Repuestos> getAllRepuestos();
+
+    Repuestos getRepuestoById(Integer id);
+
+    Repuestos saveRepuesto(Repuestos repuesto) throws RuntimeException;
+
+    Repuestos updateRepuesto(Integer id, Repuestos repuesto);
+
+    void deleteRepuesto(Integer id);
 }
