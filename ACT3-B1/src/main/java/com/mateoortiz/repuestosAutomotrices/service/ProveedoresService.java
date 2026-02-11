daa12ca7e2ec5d4659469d00a13420271f24f98c
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-10 20:48:52 -0600

    Feat: Se agrego la logica de ProveedoresService

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/ProveedoresService.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/ProveedoresService.java
@@ -1,4 +1,20 @@
 package com.mateoortiz.repuestosAutomotrices.service;
 
+import com.mateoortiz.repuestosAutomotrices.entity.Proveedor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ProveedoresService {
+
+    List<Proveedor> getAllProveedores();
+
+    Proveedor getProveedorById(Integer id);
+
+    Proveedor saveProveedor (Proveedor proveedor) throws RuntimeException;
+
+    Proveedor updateProveedor(Integer id, Proveedor proveedor);
+    void deleteProveedor(Integer id);
+
 }
