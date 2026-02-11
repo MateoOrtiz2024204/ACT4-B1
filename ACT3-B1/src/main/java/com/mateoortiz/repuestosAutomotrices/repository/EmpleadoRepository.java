a22f5cf2269b310d7e8ae0ea5752845f32f8c29f
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-10 20:45:00 -0600

    Feat: Se agrego la logica de EmpleadoRepository

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/repository/EmpleadoRepository.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/repository/EmpleadoRepository.java
@@ -1,4 +1,10 @@
 package com.mateoortiz.repuestosAutomotrices.repository;
 
-public interface EmpleadoRepository {
+import com.mateoortiz.repuestosAutomotrices.entity.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado,Integer> {
+
 }
