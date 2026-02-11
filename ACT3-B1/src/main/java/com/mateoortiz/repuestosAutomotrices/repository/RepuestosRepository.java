dcbb040b99faef09a1b6ec19c31a214da64848ef
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-11 09:53:33 -0600

    Feat: Se agrego la logica de RepuestosRepository

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/repository/RepuestosRepository.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/repository/RepuestosRepository.java
@@ -1,4 +1,9 @@
 package com.mateoortiz.repuestosAutomotrices.repository;
 
-public interface RepuestosRepository {
+import com.mateoortiz.repuestosAutomotrices.entity.Repuestos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepuestosRepository extends JpaRepository<Repuestos, Integer> {
 }
