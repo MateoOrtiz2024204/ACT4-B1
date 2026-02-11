5bf43766e46f0311cefd93f8546a813efd61f063
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-10 20:49:57 -0600

    Feat: Se agrego la logica de ProveedoresServiceImplements

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/ProveedoresServiceImplements.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/ProveedoresServiceImplements.java
@@ -1,4 +1,44 @@
 package com.mateoortiz.repuestosAutomotrices.service;
 
-public class ProveedoresServiceImplements {
+import com.mateoortiz.repuestosAutomotrices.entity.Proveedor;
+import com.mateoortiz.repuestosAutomotrices.repository.ProveedoresRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProveedoresServiceImplements implements ProveedoresService {
+    private final ProveedoresRepository proveedoresRepository;
+
+    public ProveedoresServiceImplements(ProveedoresRepository proveedoresRepository) {
+        this.proveedoresRepository = proveedoresRepository;
+    }
+
+    @Override
+    public List<Proveedor> getAllProveedores() { return proveedoresRepository.findAll(); }
+
+    @Override
+    public Proveedor getProveedorById(Integer id) { return proveedoresRepository.findById(id).orElse(null); }
+
+    @Override
+    public Proveedor saveProveedor(Proveedor proveedor) throws RuntimeException{
+        return  proveedoresRepository.save(proveedor);
+    }
+
+    @Override
+    public Proveedor updateProveedor(Integer id, Proveedor proveedor) {
+        Proveedor proveedorExist = proveedoresRepository.findById(id).orElse(null);
+        if (proveedorExist == null) {
+            return null;
+        }
+        proveedorExist.setNombreProveedor(proveedor.getNombreProveedor());
+        proveedorExist.setTelefonoProveedor(proveedor.getTelefonoProveedor());
+        proveedorExist.setDireccionProveedor(proveedor.getDireccionProveedor());
+        proveedorExist.setEmailProveedor(proveedor.getEmailProveedor());
+        return proveedoresRepository.save(proveedorExist);
+    }
+
+    @Override
+    public void deleteProveedor(Integer id) { proveedoresRepository.deleteById(id); }
+
 }
