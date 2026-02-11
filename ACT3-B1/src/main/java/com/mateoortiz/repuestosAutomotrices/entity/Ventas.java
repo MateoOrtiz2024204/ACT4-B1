c718446f57e629a150a32ed8c067769d499e8291
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-11 10:49:14 -0600

    Feat: Se agrego la logica de la entidad Ventas

---
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/entity/Ventas.java
@@ -0,0 +1,83 @@
+package com.mateoortiz.repuestosAutomotrices.entity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+
+@Entity
+@Table
+public class Ventas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_venta")
+    private Integer idVenta;
+
+    @Column(name = "fecha_venta")
+    private Date fechaVenta;
+
+    @Column(name = "cantidad")
+    private Integer cantidadRepuesto;
+
+    @Column(name = "total")
+    private Integer totalRepuesto;
+
+    //-------------------
+    @ManyToOne
+    @JoinColumn(name = "id_empleado", nullable = false)
+    private Empleado empleado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_repuesto", nullable = false)
+    private Repuestos repuestos;
+    //-------------------
+
+    public Integer getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(Date fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Integer getCantidadRepuesto() {
+        return cantidadRepuesto;
+    }
+
+    public void setCantidadRepuesto(Integer cantidadRepuesto) {
+        this.cantidadRepuesto = cantidadRepuesto;
+    }
+
+    public Integer getTotalRepuesto() {
+        return totalRepuesto;
+    }
+
+    public void setTotalRepuesto(Integer totalRepuesto) {
+        this.totalRepuesto = totalRepuesto;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Repuestos getRepuestos() {
+        return repuestos;
+    }
+
+    public void setRepuestos(Repuestos repuestos) {
+        this.repuestos = repuestos;
+    }
+}
